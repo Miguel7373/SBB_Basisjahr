324358fd30d7ae7859263261c6a682f81872cefb
Miguel Lehmann <lehmann@puzzle.ch>
2023-08-30 15:50:40 +0200

    overhal

--- labs_J1/aufgabe2.java
+++ labs_J1/aufgabe2.java
@@ -1,31 +1,58 @@
 package labs_J1;
 import java.util.Scanner;
 public class aufgabe2 {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Gib eine nummer ein");
-            String str = sc.nextLine();
-            if (str.contains("miguel")){
-                System.out.println("Hello Miguel");
-                System.exit(0);
-            }
-            double zahl = Double.parseDouble(str);
-            if (zahl % 4 == 0) {
-                System.out.println("das ist ein Schaltjahr");
-            } else {
-                System.out.println("das ist kein schaltjahr");
-            }
-            if (zahl % 2 == 0) {
-            } else {
-                System.out.println("die zahl ist ungerade");
-            }
-            if (str.equals("0")) {
-                System.out.println("equals 0 ");
-            } else if (zahl > 0 ) {
-                System.out.println("die zahl ist grösser als null");
-            } else if (zahl < 0 ) {
-                System.out.println("die zahl ist kleiner als null");
-            }
+
+    public static String Name(String Miguel) {
+        if (Miguel.contains("Miguel")) {
+            System.out.println("Hello Miguel");
+            System.exit(0);
         }
+        return Miguel ;
+    }
+    public static void main(String[] args) {
+        if (Name().contains("Miguel")) {
+            System.out.println("Hello Miguel");
+            System.exit(0);
+        }
+
+        public static Integer Schaltjahr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gib eine nummer ein");
+        String str = sc.nextLine();
+        double zahl = Double.parseDouble(str);
+        if (zahl % 4 == 0) {
+            System.out.println("das ist ein Schaltjahr");
+        } else {
+            System.out.println("das ist kein schaltjahr");
+        }
+          return (Schaltjahr());
+    }
+
+    public static Integer ungerade() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gib eine nummer ein");
+        String str = sc.nextLine();
+        double zahl = Double.parseDouble(str);
+        if (zahl % 2 == 0) {
+        } else {
+            System.out.println("die zahl ist ungerade");
+        }
+        return ungerade();
+    }
+
+    public static Integer equals() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gib eine nummer ein");
+        String str = sc.nextLine();
+        double zahl = Double.parseDouble(str);
+
+        if (str.equals("0")) {
+            System.out.println("equals 0 ");
+        } else if (zahl > 0) {
+            System.out.println("die zahl ist grösser als null");
+        } else if (zahl < 0) {
+            System.out.println("die zahl ist kleiner als null");
+        }
+        return equals();
     }
 }
